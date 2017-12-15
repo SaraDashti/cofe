0fde0c82b73a276fa89eee5e4017097564c8cc52
SaraDashti <cpe.dashti@gmail.com>
2017-12-15 20:04:11 +0300

    signup, log

--- jewcofe/urls.py
+++ jewcofe/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path
+
+app_name = 'jewcofe'
 
 urlpatterns = [
-    path('signup/', views.usersignup, name="signup"),
-    path('login/', views.userlogin, name="login"),
-    path('logout/', views.userlogout, name="logout"),
+    path('signup/', views.usersignup),
+    path('login/', views.userlogin,),
+    path('logout/', views.userlogout),
 ]
